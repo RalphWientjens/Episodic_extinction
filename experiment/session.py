f400f88045d71a69a1ccaafdd4b53587c9d0b2eb
RalphWientjens <ralphw713@hotmail.com>
2026-02-13 13:10:27 +0100

    new instructions

--- experiment/session.py
+++ experiment/session.py
@@ -415,6 +415,9 @@ class ExtinctionSession(PylinkEyetrackerSession):
             # self.clock.reset()  # reset clock after practice
 
         else:
+            self.show_text_screen(
+                self.instructions[f"session_{self.sess}"]["Start instructions"]
+            )
             #start experiment timing for sessions 2 and 3
             self.start_experiment()
 
